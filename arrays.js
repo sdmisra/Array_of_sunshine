ee7e096690aa4b4b7a633ca2346f1b7566798c15
Shane Misra <shane.misra@gmail.com>
2022-11-09 16:37:25 -0700

    Finish all but eval

--- arrays.js
+++ arrays.js
@@ -31,3 +31,10 @@ console.log(factsBooleans)
 // In programming, counting generally begins from 0, rather than from 1. That means that the first value within an array is considered to be at index position 0. The second value at 1, etc. We can also use negative numbers to begin counting from the end of the array, such as using the index position of -1 to indicate the final position in the array.
 
 // We can practice this concept by printing to the console and guessing what the output will be based off of this understanding.
+
+// Prediction : console.log(ageNumbers[0]) will return 32 (as a number)
+console.log(ageNumbers[0])
+console.log(typeof(ageNumbers[0]))
+// Prediction : console.log(factsBooleans[2]) will return false (boolean).
+console.log(factsBooleans[2])
+console.log(typeof(factsBooleans[2]))
